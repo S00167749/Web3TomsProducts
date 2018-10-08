c3b68667b28033234eb35a6b1bd48aea7ddec239
S00167749 <S00167749@VLAB-PC-57>
2018-10-08 09:51:39 +0100

    added catcherror with pipe and tap

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { IProduct } from '../IProduct';
 import { Observable } from 'rxjs';
 import { HttpErrorResponse,HttpClient } from '@angular/common/http';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-import { tap } from 'rxjs/operators';
+//import 'rxjs/add/observable/throw';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +15,9 @@ export class ProductService {
   constructor(private _http : HttpClient) { }
 
   getProducts(): Observable<IProduct[]> {
-    return this._http.get<IProduct[]>(this._productUrl).tap(
-      data => console.log('All : ' + JSON.stringify(data)))
-      .catch(this.handleError);
+    return this._http.get<IProduct[]>(this._productUrl).pipe(
+      tap(data => console.log('All : ' + JSON.stringify(data)))
+      ,catchError(this.handleError));
   }
 
   private handleError(err:HttpErrorResponse){
